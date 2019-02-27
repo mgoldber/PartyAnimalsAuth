b30351c30d0e52f78850f5c3222e36da7d9aede3
MarkerYou <markeryou@Marks-MacBook-Pro-2.local>
2019-02-27 17:06:17 -0500

    Updates

---
+++ precache-manifest.5c519166016571c0fcf93fbad9e10a00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d1638dd0e43812f346c9",
+    "url": "/PartyAnimalsAuthDocs/static/css/main.fd0dd1ae.chunk.css"
+  },
+  {
+    "revision": "d1638dd0e43812f346c9",
+    "url": "/PartyAnimalsAuthDocs/static/js/main.d1638dd0.chunk.js"
+  },
+  {
+    "revision": "d018f197c1f37b3b59df",
+    "url": "/PartyAnimalsAuthDocs/static/js/runtime~main.d018f197.js"
+  },
+  {
+    "revision": "ca8f0d53fd7fee91a2f2",
+    "url": "/PartyAnimalsAuthDocs/static/js/2.ca8f0d53.chunk.js"
+  },
+  {
+    "revision": "a1869bf4d4e10b779af8482c09e3ba91",
+    "url": "/PartyAnimalsAuthDocs/index.html"
+  }
+];
